ecaaa9b2b094dc4e40fe761d4a55bed0d38264c2
Christopher Dengsø <chrdengso@gmail.com>
2017-10-24 16:05:01 +0800

    mongo url

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,6 +1,6 @@
 export default {
   env: process.env.NODE_ENV || 'production',
-  db: process.env.DB || 'mongodb://localhost/prod',
+  db: process.env.MONGO_URL || 'mongodb://localhost/prod',
   port: process.env.PORT || 4040,
   auth_domain: process.env.AUTH_DOMAIN || '',
 };
