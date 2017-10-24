2d80f611e89d00df58539d1afdd78e8770f80d19
Christopher Dengsø <chrdengso@gmail.com>
2017-10-24 17:43:15 +0800

    now listening

--- server/index.js
+++ server/index.js
@@ -31,13 +31,10 @@ if (config.MONGOOSE_DEBUG) {
   });
 }
 
-// module.parent check is required to support mocha watch
-// src: https://github.com/mochajs/mocha/issues/1912
-if (!module.parent) {
-  // listen on port config.port
-  app.listen(config.port, () => {
-    debug(`server started on port ${config.port} (${config.env})`);
-  });
-}
+// listen on port config.port
+app.listen(config.port, () => {
+  debug(`server started on port ${config.port} (${config.env})`);
+});
+
 
 export default app;
