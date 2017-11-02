4dab7504aac848515eb56270833078eecbb05f35
Christopher Dengsø <chrdengso@gmail.com>
2017-11-02 21:16:55 +0800

    cueup api env var

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -3,5 +3,5 @@ export default {
   db: process.env.MONGO_URL || 'mongodb://localhost/prod',
   port: process.env.PORT || 4040,
   auth_domain: process.env.AUTH_DOMAIN || '',
-  cueup_api_domain: 'https://api.cueup.io'
+  cueup_api_domain: process.env.CUEUP_API_DOMAIN || 'https://api.cueup.io'
 };
