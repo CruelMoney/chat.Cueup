002ccaee6dc1094dc9bacb0c335cc83f3b8d508e
Christopher Dengsø <chrdengso@gmail.com>
2017-11-20 11:17:24 +0800

    env

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -6,5 +6,5 @@ export default {
   db: process.env.MONGO_URL || 'mongodb://localhost/dev',
   port: process.env.PORT || 4040,
   auth_domain: process.env.AUTH_DOMAIN || '',
-  cueup_api_domain: 'https://staging.api.cueup.io'
+  cueup_api_domain: process.env.CUEUP_API_DOMAIN || 'https://staging.api.cueup.io'
 };
